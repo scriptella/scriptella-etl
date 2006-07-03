74d255a9ce02f955d6350a7e207384d6923e5560
ejboy <ejboy@cb111aed-9b11-0410-92cd-84c162621750>
2006-07-03 20:56:30 +0000

    Dialect database renamed to name

--- trunk/drivers/src/test/scriptella/drivers/hsql/ShutdownOnExitTest.java
+++ trunk/drivers/src/test/scriptella/drivers/hsql/ShutdownOnExitTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package scriptella.spi.hsql;
+package scriptella.drivers.hsql;
 
 import scriptella.AbstractTestCase;
 import scriptella.jdbc.JDBCConnection;
