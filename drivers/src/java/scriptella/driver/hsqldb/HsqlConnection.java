aa066b89f688f49839a17dc20b791bd667dd602d
Fyodor Kupolov <ejboy@users.noreply.github.com>
2006-07-31 20:29:51 +0000

    Renamed package hsql to hsqldb, site chnages

--- drivers/src/java/scriptella/driver/hsqldb/HsqlConnection.java
+++ drivers/src/java/scriptella/driver/hsqldb/HsqlConnection.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package scriptella.driver.hsql;
+package scriptella.driver.hsqldb;
 
 import scriptella.jdbc.JDBCConnection;
 import scriptella.jdbc.JDBCUtils;
