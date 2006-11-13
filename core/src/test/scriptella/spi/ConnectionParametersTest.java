4fec915ef29cdb0911e09bd2ee5c4f44641458ff
Fyodor Kupolov <ejboy@users.noreply.github.com>
2006-11-13 22:59:43 +0000

    fix for parsing numeric connection parameters

---
+++ core/src/test/scriptella/spi/ConnectionParametersTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2006 The Scriptella Project Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package scriptella.spi;
+
+import scriptella.AbstractTestCase;
+
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Tests for {@link ConnectionParameters}.
+ *
+ * @author Fyodor Kupolov
+ * @version 1.0
+ */
+public class ConnectionParametersTest extends AbstractTestCase {
+    /**
+     * Tests if properties parsing methods work correctly.
+     */
+    public void testPropertiesParsing() throws ParseException {
+        Map<String,Object> p = new HashMap<String, Object>();
+        p.put("int",10);
+        p.put("negative",-10);
+        p.put("int2","20");
+        p.put("boolean",true);
+        p.put("boolean2","yes");
+        ConnectionParameters cp = new ConnectionParameters(p, "", "", "", "", "", MockDriverContext.INSTANCE);
+        Integer v = cp.getIntegerProperty("nosuchproperty", 1);
+        assertEquals(1, v.intValue());
+        v = cp.getIntegerProperty("int", 1);
+        assertEquals(10, v.intValue());
+        v = cp.getIntegerProperty("negative", 1);
+        assertEquals(-10, v.intValue());
+        v = cp.getIntegerProperty("int2", 1);
+        assertEquals(20, v.intValue());
+        boolean b = cp.getBooleanProperty("nosuchprop", true);
+        assertEquals(true, b);
+        b = cp.getBooleanProperty("boolean", false);
+        assertEquals(true, b);
+        b = cp.getBooleanProperty("boolean2", false);
+        assertEquals(true, b);
+
+
+    }
+}
