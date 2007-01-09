6aaa00e9d0c2a78ba9a76c4d4264e1ee48d846f2
ejboy <ejboy@cb111aed-9b11-0410-92cd-84c162621750>
2007-01-09 17:43:38 +0000

    #4308 Initial codebase for XML XPath provider

---
+++ trunk/drivers/src/java/scriptella/driver/xpath/XPathQueryExecutor.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2006 The Scriptella Project Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package scriptella.driver.xpath;
+
+import org.w3c.dom.Node;
+import scriptella.spi.ParametersCallback;
+
+public class XPathQueryExecutor implements ParametersCallback {
+    private Node node;
+
+    public Object getParameter(final String name) {
+        return null;
+    }
+}
