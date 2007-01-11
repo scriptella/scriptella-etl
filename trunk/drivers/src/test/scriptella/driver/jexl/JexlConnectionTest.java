b888eda232dd3e34382ab2ec24db4a034119d141
ejboy <ejboy@cb111aed-9b11-0410-92cd-84c162621750>
2007-01-11 21:45:44 +0000

    #3792 Test for JEXL connection

---
+++ trunk/drivers/src/test/scriptella/driver/jexl/JexlConnectionTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2006-2007 The Scriptella Project Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package scriptella.driver.jexl;
+
+import scriptella.AbstractTestCase;
+import scriptella.configuration.MockConnectionEl;
+import scriptella.configuration.StringResource;
+import scriptella.spi.ConnectionParameters;
+import scriptella.spi.MockDriverContext;
+import scriptella.spi.MockParametersCallbacks;
+import scriptella.spi.Resource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Tests for {@link JexlConnection}.
+ *
+ * @author Fyodor Kupolov
+ * @version 1.0
+ * @since 11.01.2007
+ */
+public class JexlConnectionTest extends AbstractTestCase {
+    private Object v;
+    public void setValue(Object v) {
+        this.v = v;
+
+    }
+
+    public void testExecuteScript() {
+        v=null;
+        Resource r = new StringResource("x=0;while (x < 10) {x=x+2;};obj.setValue(x)");
+        JexlConnection jc = new JexlConnection(new ConnectionParameters(new MockConnectionEl(), new MockDriverContext()));
+        Map<String, Object> m = new HashMap<String, Object>();
+        m.put("obj", this);
+        jc.executeScript(r, MockParametersCallbacks.fromMap(m));
+        assertEquals(10, ((Number)v).intValue());
+
+    }
+}
