9dcd13b5eabdffabcb4cf6a0b2262c05164f7a00
ejboy <ejboy@cb111aed-9b11-0410-92cd-84c162621750>
2007-04-06 13:47:59 +0000

    #5253

--- trunk/tools/src/java/scriptella/tools/template/DataMigrator.java
+++ trunk/tools/src/java/scriptella/tools/template/DataMigrator.java
@@ -17,8 +17,11 @@ package scriptella.tools.template;
 
 import scriptella.jdbc.GenericDriver;
 import scriptella.jdbc.JdbcException;
+import scriptella.jdbc.JdbcUtils;
 import scriptella.spi.ConnectionParameters;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
@@ -29,7 +32,10 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * TODO: Add documentation
@@ -38,17 +44,37 @@ import java.util.Set;
  * @version 1.0
  */
 public class DataMigrator extends TemplateManager {
+    private static final String DATA_MIGRATOR_ETL_XML = "dataMigrator.etl.xml";
+    private static final String DATA_MIGRATOR_BLOCK_ETL_XML = "dataMigratorBlock.etl.xml";
+    private static Logger LOG = Logger.getLogger(DataMigrator.class.getName());
+    static boolean DEBUG = LOG.isLoggable(Level.FINE);
+
+
+
+    public void create(Map<String, ?> properties) throws IOException {
+        String baseName = defineName();
+        String xmlName = baseName + XML_EXT;
+        String propsName = baseName + PROPS_EXT;
+
+        String etlXml = loadResourceAsString(DATA_MIGRATOR_ETL_XML);
+        String block = loadResourceAsString(DATA_MIGRATOR_BLOCK_ETL_XML);
+        Writer w = newFileWriter(xmlName);
+
+
+    }
+
 
     public static void main(final String args[]) {
         //Currently this class is just a sample of how to generate a migration script template
         //Rewite it
 
-        ConnectionParameters params = new ConnectionParameters(null,null);
+        ConnectionParameters params = new ConnectionParameters(null, null);
         GenericDriver jdbcDriver = new GenericDriver();
 
 
         final Connection con = jdbcDriver.connect(params).getNativeConnection();
-        final Set<String> tables = sortTables(con, params);
+        DbSchema schema = new DbSchema(con, "", "");
+        final Set<String> tables = sortTables(schema);
         StringBuilder o = new StringBuilder();
         o.append("<etl>\n" +
                 "    <connection id=\"in\" driver=\"com.sybase.jdbc2.jdbc.SybDriver\" url=\"jdbc:sybase:Tds:localhost:2638\" user=\"DBA\" password=\"SQL\"/>\n" +
@@ -57,14 +83,14 @@ public class DataMigrator extends TemplateManager {
         for (String t : tables) {
             o.append("    <query connection-id=\"in\">\n")
                     .append("      select ");
-            appendColumnNames(con, params, t, o).append(" from ").append(t);
+            appendColumnNames(schema, t, o).append(" from ").append(t);
 
             o.append("      <script connection-id=\"out\">\n")
                     .append("         insert into ").append(t).append("(");
-            appendColumnNames(con, params, t, o).append(") VALUES (");
-            appendColumnNames(con, params, t, o, ", ", "?").append(")")
+            appendColumnNames(schema, t, o).append(") VALUES (");
+            appendColumnNames(schema, t, o, ", ", "?").append(")")
                     .append("\n      </script>\n").
-            append("\n    </query>\n");
+                    append("\n    </query>\n");
         }
 
         o.append("</etl>\n");
@@ -73,15 +99,13 @@ public class DataMigrator extends TemplateManager {
         //        JdbcUtils.getTableColumns(con, c,)
     }
 
-    private static StringBuilder appendColumnNames(
-            final Connection con, ConnectionParameters params, final String table, final StringBuilder sql) {
-        return appendColumnNames(con, params, table, sql, ", ", "");
+    private static StringBuilder appendColumnNames(DbSchema schema, final String table, final StringBuilder sql) {
+        return appendColumnNames(schema, table, sql, ", ", "");
     }
 
-    private static StringBuilder appendColumnNames(
-            final Connection con, ConnectionParameters params, final String table,
-            final StringBuilder sql, final String separator, final String prefix) {
-        final Set<String> tableColumns = getTableColumns(con, params, table);
+    private static StringBuilder appendColumnNames(DbSchema schema, final String table,
+                                                   final StringBuilder sql, final String separator, final String prefix) {
+        final Set<String> tableColumns = schema.getTableColumns(table);
 
         for (Iterator<String> it = tableColumns.iterator(); it.hasNext();) {
             String s = it.next();
@@ -96,49 +120,38 @@ public class DataMigrator extends TemplateManager {
         return sql;
     }
 
-    private static Set<String> sortTables(final Connection con, ConnectionParameters params) {
-        List<String> tables = getTables(con, params);
-        System.out.println("Sorting " + tables);
+    private static Set<String> sortTables(final DbSchema schema) {
+        List<String> tables = schema.getTables();
+        LOG.fine("Sorting " + tables);
 
         int n = tables.size();
-        int m[][] = new int[n][n];
-
-        for (int i = 0; i < m.length; i++) {
-            Arrays.fill(m[i], 0);
-        }
 
         String tbls[] = tables.toArray(new String[n]);
+        int[][] m;
 
         try {
-            final DatabaseMetaData metaData = con.getMetaData();
-
-            for (int i = 0; i < n; i++) {
-                String t = tbls[i];
-                final ResultSet rs = metaData.getExportedKeys(params.getCatalog(),
-                        params.getSchema(), t);
-
-                while (rs.next()) {
-                    //                    if (rs.getInt(11) != 2) { //Only not null FKs are taken into account
-                    String t2 = rs.getString(7);
-                    int i2 = indexOf(tbls, t2);
-
-                    if (i2 >= 0) {
-                        m[i][i2] += ((rs.getInt(11) != 2) ? 10 : 1);
-                    }
-
-                    //                    }
-                }
-
-                rs.close();
+            try {
+                m=getTablesMatrix(schema, tbls);
+            } catch (SQLException e) { //on error try alternative algorithm
+                LOG.log(Level.WARNING, "Unable to define order of tables, trying the simplified algorithm");
+                m=getAlternativeTablesMatrix(schema, tbls);
             }
+            StringBuilder msg = DEBUG?new StringBuilder() : null;
+
 
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    System.out.print(m[i][j]);
-                    System.out.print((m[i][j] >= 10) ? " " : "  ");
+                    if (DEBUG) {
+                        msg.append(m[i][j]);
+                        msg.append((m[i][j] >= 10) ? " " : "  ");
+                    }
                 }
-
-                System.out.println(tbls[i]);
+                if (DEBUG) {
+                    msg.append(tbls[i]).append('\n');
+                }
+            }
+            if (DEBUG) {
+                LOG.fine("Tables dependencies matrix: \n"+msg);
             }
 
             boolean free[] = new boolean[n];
@@ -147,7 +160,7 @@ public class DataMigrator extends TemplateManager {
             Set<String> res = new LinkedHashSet<String>();
 
             for (int i = 0; i < n; i++) {
-                //on each i iteration we choose the best candidate (having minimal relationships)
+                //on each i iteration we choose the best candidate (having minimum number of incoming relationships)
                 int min = Integer.MAX_VALUE;
                 int minI = -1;
 
@@ -182,6 +195,60 @@ public class DataMigrator extends TemplateManager {
         }
     }
 
+    private static int[][] getTablesMatrix(DbSchema schema, String[] tables) throws SQLException {
+        final DatabaseMetaData metaData = schema.getMetaData();
+        int n = tables.length;
+        int m[][] = new int[n][n];
+
+        for (int[] a : m) {
+            Arrays.fill(a, 0);
+        }
+
+        for (int i = 0; i < n; i++) {
+            final ResultSet rs = metaData.getExportedKeys(schema.getCatalog(), schema.getSchema(), tables[i]);
+            while (rs.next()) {
+                String t2 = rs.getString("FKTABLE_NAME");
+                int i2 = indexOf(tables, t2);
+
+                if (i2 >= 0) {
+                    m[i][i2] += ((rs.getInt("DELETE_RULE") != 2) ? 10 : 1);
+                }
+            }
+
+            rs.close();
+        }
+        return m;
+    }
+
+    static int[][] getAlternativeTablesMatrix(DbSchema schema, String[] tables) throws SQLException {
+        int n = tables.length;
+        int m[][] = new int[n][n];
+        for (int[] a : m) {
+            Arrays.fill(a, 0);
+        }
+
+        Set[] pks = new Set[n];
+        for (int i = 0; i < n; i++) {
+            pks[i] = schema.getPrimaryKeys(tables[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            Set<String> columns = schema.getTableColumns(tables[i]);
+            for (String column : columns) { //Iterate through all non-PK columns
+                if (!pks[i].contains(column)) {
+                    //Search for tables which export foreign keys into tables[i]
+                    for (int j = 0; j != i && j < n; j++) {
+                        if (pks[j].contains(column)) {
+                            m[j][i]+=1;
+                        }
+                    }
+                }
+            }
+        }
+        return m;
+    }
+
+
     private static int indexOf(final String list[], final String element) {
         for (int i = 0; i < list.length; i++) {
             if (list[i].equalsIgnoreCase(element)) {
@@ -192,45 +259,89 @@ public class DataMigrator extends TemplateManager {
         return -1;
     }
 
-    private static List<String> getTables(Connection con, ConnectionParameters params) {
-        try {
-            return getColumn(con.getMetaData()
-                    .getTables(con.getCatalog(),
-                    params.getSchema(), null, new String[]{"TABLE"}), 3);
-        } catch (SQLException e) {
-            throw new JdbcException(e.getMessage(), e);
+    static class DbSchema {
+        private Connection connection;
+        private DatabaseMetaData metaData;
+        private String catalog;
+        private String schema;
+
+        public DbSchema(Connection connection, String catalog, String schema) {
+            this.connection = connection;
+            this.catalog = catalog;
+            this.schema = schema;
         }
-    }
 
-    private static Set<String> getTableColumns(Connection con, ConnectionParameters params, final String tableName) {
-        try {
-            return new HashSet<String>(getColumn(
-                    con.getMetaData()
-                            .getColumns(params.getCatalog(), params.getSchema(), tableName, null),
-                    4));
-        } catch (SQLException e) {
-            throw new JdbcException(e.getMessage(), e);
-        }
-    }
 
-    /**
-     * Iterates through the resultset and returns column values.
-     * @param rs resultset to iterate.
-     * @param columnPos column position. Starts at 1.
-     * @return list of column values.
-     */
-    public static List<String> getColumn(final ResultSet rs, final int columnPos) {
-        List<String> l = new ArrayList<String>();
-
-        try {
-            while (rs.next()) {
-                l.add(rs.getString(columnPos));
+        List<String> getTables() {
+            try {
+                return getColumn(getMetaData()
+                        .getTables(catalog, schema, null, new String[]{"TABLE"}), 3);
+            } catch (SQLException e) {
+                throw new JdbcException(e.getMessage(), e);
             }
-        } catch (SQLException e) {
-            throw new JdbcException("Unable to get column #" + columnPos, e);
         }
 
-        return l;
+        Set<String> getPrimaryKeys(final String tableName) {
+            try {
+                return new HashSet<String>(
+                        getColumn(getMetaData().getPrimaryKeys(catalog, schema, tableName), 6));
+            } catch (SQLException e) {
+                throw new JdbcException(e.getMessage(), e);
+            }
+        }
+
+        Set<String> getTableColumns(final String tableName) {
+            try {
+                return new HashSet<String>(
+                        getColumn(getMetaData().getColumns(catalog, schema, tableName, null), 4));
+            } catch (SQLException e) {
+                throw new JdbcException(e.getMessage(), e);
+            }
+        }
+
+
+        /**
+         * Iterates through the resultset and returns column values.
+         *
+         * @param rs        resultset to iterate.
+         * @param columnPos column position. Starts at 1.
+         * @return list of column values.
+         */
+        static List<String> getColumn(final ResultSet rs, final int columnPos) {
+            List<String> l = new ArrayList<String>();
+            try {
+                while (rs.next()) {
+                    l.add(rs.getString(columnPos));
+                }
+            } catch (SQLException e) {
+                throw new JdbcException("Unable to get column #" + columnPos, e);
+            } finally {
+                JdbcUtils.closeSilent(rs);
+            }
+
+            return l;
+        }
+
+        public DatabaseMetaData getMetaData() {
+            if (metaData == null) {
+                try {
+                    metaData = connection.getMetaData();
+                } catch (SQLException e) {
+                    throw new JdbcException("Unable to get database metadata");
+                }
+            }
+            return metaData;
+        }
+
+
+        public String getCatalog() {
+            return catalog;
+        }
+
+        public String getSchema() {
+            return schema;
+        }
     }
 
 }
+
