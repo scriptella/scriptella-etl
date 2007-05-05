766d5819fbac6d703d89f232bdf5480a1ff8c1c1
ejboy <ejboy@cb111aed-9b11-0410-92cd-84c162621750>
2007-05-05 14:00:47 +0000

    Improved javadoc Caching notes for #5473

--- trunk/core/src/java/scriptella/spi/Connection.java
+++ trunk/core/src/java/scriptella/spi/Connection.java
@@ -25,7 +25,10 @@ package scriptella.spi;
  */
 public interface Connection {
     /**
-     * @return dialect identifier for this connection.
+     * This method returns a language dialect identifier for this connection.
+     *
+     * @return dialect identifier.
+     * @throws ProviderException if driver failed to determine a language dialect.
      */
     DialectIdentifier getDialectIdentifier() throws ProviderException;
 
@@ -33,9 +36,12 @@ public interface Connection {
      * Executes a script specified by its content.
      * <p>scriptContent may be used as a key for caching purposes, i.e.
      * provider may precompile scripts and use compiled versions for subsequent executions.
+     * Please note that <em>only inline {@link scriptella.configuration.StringResource text resources}
+     * can be safely cached</em>.
      *
      * @param scriptContent      script content. Cannot be null.
      * @param parametersCallback callback to get parameter values. Cannot be null.
+     * @throws ProviderException if script execution failed.
      */
     void executeScript(Resource scriptContent, ParametersCallback parametersCallback) throws ProviderException;
 
@@ -46,7 +52,8 @@ public interface Connection {
      * @param queryContent       query content. Cannot be null.
      * @param parametersCallback callback to get parameter values. Cannot be null.
      * @param queryCallback      callback to call for each result set element produced by this query. Cannot be null.
-     * @see #executeScript(scriptella.spi.Resource, scriptella.spi.ParametersCallback)
+     * @throws ProviderException if query execution failed.
+     * @see #executeScript(scriptella.spi.Resource,scriptella.spi.ParametersCallback)
      */
     void executeQuery(Resource queryContent, ParametersCallback parametersCallback, QueryCallback queryCallback) throws ProviderException;
 
@@ -54,7 +61,7 @@ public interface Connection {
      * This method returns the number of executed statements or 0 if this feature is unsupported.
      * <p>If possible the connection should collect statistics about the number of executed statement.
      * It's recommended to provide the most actual execution statistics, i.e. increment internal statements
-     * counter during a script or a query execution, so the monitoring tools would be able to track progress.  
+     * counter during a script or a query execution, so the monitoring tools would be able to track progress.
      *
      * @return number of executed statements or 0 if this feature is unsupported.
      */
@@ -62,12 +69,16 @@ public interface Connection {
 
     /**
      * Commits a current transaction (if any).
+     * <p>Throwing an error during commit phase cause {@link #rollback() rollback}.
+     *
+     * @throws ProviderException if a problem occured during commit phase.
      */
     void commit() throws ProviderException;
 
     /**
      * Rolls back a current transaction (if any).
-     * @throws ProviderException if driver fails to roll back a transaction.
+     *
+     * @throws ProviderException             if driver fails to roll back a transaction.
      * @throws UnsupportedOperationException if transactions are not supported
      */
     void rollback() throws ProviderException, UnsupportedOperationException;
@@ -75,10 +86,12 @@ public interface Connection {
 
     /**
      * Closes the connection and releases all related resources.
+     *
+     * @throws ProviderException if a critical failure occured.
      */
     void close() throws ProviderException;
 
-     /**
+    /**
      * @return meaningful label for connection
      */
     String toString();
